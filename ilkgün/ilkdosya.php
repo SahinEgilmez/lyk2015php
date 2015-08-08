9b7bbe6246176784e991b227480ade3c564521d3
turkas64 <turkas64@gmail.com>
2015-08-08 19:41:32 +0300

    ilk gün denemesi

---
+++ ilkgün/ilkdosya.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8" />
+<?php
+//$a =22;
+//settype($a,"string");
+
+//echo gettype($a);
+
+$sayi1=5;
+$sayi2=3;
+
+//echo $sayi1 + $sayi2;
+//echo floor ($sayi1 / $sayi2);//aşağı yuvarla
+//echo ceil ($sayi1 * $sayi2);//yukarı yuvarla
+//echo $sayi1 - $sayi2;
+//echo $sayi1 % $sayi2;
+
+//echo ++$sayi1;
+
+echo $$sayi2;
+
+
+?>
